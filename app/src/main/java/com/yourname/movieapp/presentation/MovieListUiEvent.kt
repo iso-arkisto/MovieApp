a4d1d47ac13389a1b956151ccfce1c32fec54700
Andrey <Fian.123@yandex.ru>
2026-07-28 14:24:26 +0300

    fix: fix category switching logic in MovieListViewModel

--- app/src/main/java/com/yourname/movieapp/presentation/MovieListUiEvent.kt
+++ app/src/main/java/com/yourname/movieapp/presentation/MovieListUiEvent.kt
@@ -3,7 +3,9 @@ package com.yourname.movieapp.presentation
 sealed interface MovieListUiEvent {
     data class Paginate(
         val category: String
-    ): MovieListUiEvent
+    ) : MovieListUiEvent
 
-    object Navigate: MovieListUiEvent
+    data class ChangeCategory(
+        val category: String
+    ) : MovieListUiEvent
 }
